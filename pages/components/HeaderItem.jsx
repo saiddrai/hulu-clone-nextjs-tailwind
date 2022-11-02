bcb8d9486a1b604641344b14fa9ffb2aec04cac0
saiddrai <saiddrai42@gmail.com>
2022-11-02 19:24:33 +0100

    header

--- pages/components/HeaderItem.jsx
+++ pages/components/HeaderItem.jsx
@@ -1,9 +1,9 @@
-const HeaderItem = ({title, Icon}) => {
+    const HeaderItem = ({title, Icon}) => {
     return ( 
         <div className=" flex flex-col items-center
         w-12 sm:w-20 hover:text-white cursor-pointer group">
-            <Icon className="h-8 mb-1" />
-            <p className="opacity-50 group-hover:opacity-100
+            <Icon className="h-8 mb-1 group-hover:animate-bounce " />
+            <p className="opacity-0 group-hover:opacity-100
              tracking-widest" >{title}</p>
         </div>
      );
